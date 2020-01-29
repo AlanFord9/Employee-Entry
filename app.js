c56821b7b134b9cf70e8c3d21b8ccf3f1ebf9138
frankcodes <alanforducf@gmail.com>
2020-01-28 19:31:50 -0500

    hid api info and linked to database

---
+++ app.js
@@ -0,0 +1,22 @@
+var database = firebase.database();
+
+var connectionsRef = database.ref("/connections");
+
+// '.info/connected' is a special location provided by Firebase that is updated every time
+// the client's connection state changes.
+// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+var connectedRef = database.ref(".info/connected");
+
+// When the client's connection state changes...
+connectedRef.on("value", function(snap) {
+
+  // If they are connected..
+  if (snap.val()) {
+
+    // Add user to the connections list.
+    var con = connectionsRef.push(true);
+
+    // Remove user from the connection list when they disconnect.
+    con.onDisconnect().remove();
+  }
+});
